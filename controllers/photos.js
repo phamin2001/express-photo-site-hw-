625b255395ca46d41a5f1ab2365c5162daae0964
phamin2001 <amin.pahlavani@gmail.com>
2019-01-23 06:53:20 -0700

    commit show route

--- controllers/photos.js
+++ controllers/photos.js
@@ -9,10 +9,27 @@ router.get('/',(req, res) => {
         if(err) {
             res.send(err);
         } else {
-            res.render('photos/index.ejs');
+            res.render('photos/index.ejs', {
+                photos: allPhotos
+            });
         }
     });
 });
+
+// show route
+router.get('/:id', (req, res) => {
+    Photo.findById(req.params.id, (err, foundPhoto) => {
+        if(err) {
+            res.send(err);
+        } else {
+            res.render('photos/show.ejs', {
+                photo: foundPhoto
+            });
+        }
+    });
+});
+
+
 
 module.exports = router;
 
